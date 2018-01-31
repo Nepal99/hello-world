e4f3426db161a87c3392be25ec420fea93b6c49d
Nepal99 <reddy.nepal571@gmail.com>
2018-01-31 17:01:43 -0500

    3- 34 & 35 - Templates and directives

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
     selector:'courses',
-    template: '<h2>Courses</h2>'
+    template: `
+    <h2>Courses</h2>
+    <h2>{{"Title : " + title}}</h2>
+    <h3>{{getTitle()}}</h3>
+    <ul>
+        <li *ngFor="let course of courses">{{course}}</li>
+    </ul>
+    `
 })
 
 export class CoursesComponent {
-
+    title = 'List of courses';
+    courses =['Course1','Course2','Course3',];
+    getTitle(){
+        return this.title;
+    }
 }
