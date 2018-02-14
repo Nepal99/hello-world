53eb41df639ebe91305bbf47edf0774fae2f4e66
Nepal99 <reddy.nepal571@gmail.com>
2018-02-14 14:47:08 -0500

    3-36, 37,38 -
    -> Created a custom service,
          -> Many ways to create services
    -> Understand the dependency Injection
           -> Why and how to give the depemdency injection
    -> Create Services using CLI
             -> Unerstand @injectable

--- src/app/courses.component.ts
+++ src/app/courses.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { CoursesService } from './courses.service';
 
 @Component({
-    selector:'courses',
-    template: '<h2>Courses</h2>'
+    selector: 'courses',
+    template: `<h2>Courses : {{title}}</h2>
+                <ul>
+                    <li *ngFor="let course of courses">{{course}}</li>
+                </ul>          
+    `
 })
 
 export class CoursesComponent {
+
+    title = "List of courses";
+    // courses = ['Course1', 'Course2', 'Course3', 'Course4'];
+    courses;
+
+    /*
+     Another way importing and using the services. 
+     constructor() {
+         let service = new CoursesService();
+         this.courses = service.getCourses();
+     } */
+    /**
+     * 
+     * @param service 
+     * Here we are passing the CoursesService as the dependency to the component.
+     * We have to provide this dependecy in the app.module.ts in providers[]
+     */
+    constructor(service: CoursesService) {
+        this.courses = service.getCourses();
+    }
 
 }
